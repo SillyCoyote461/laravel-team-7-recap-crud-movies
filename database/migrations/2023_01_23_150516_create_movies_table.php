dcf3891740276ddcb8b00b38f9e724cd540d5ad6
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-23 17:39:16 +0100

    Creata migration

--- database/migrations/2023_01_23_150516_create_movies_table.php
+++ database/migrations/2023_01_23_150516_create_movies_table.php
@@ -15,6 +15,9 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('vote');
             $table->timestamps();
         });
     }
