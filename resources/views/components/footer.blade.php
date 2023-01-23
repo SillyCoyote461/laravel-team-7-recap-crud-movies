21c035df0b206d2069d8b452c8bb45d55ac7916d
marcodldev <marcodelisi2@gmail.com>
2023-01-23 17:16:24 +0100

    + style

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-body-tertiary footer1 ">
+    <div class="container">
+        <h4 class="p-3">Footer brutto e inutile</h4>
+    </div>
+
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html >
+<html lang="en" >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +7,13 @@
     <title>@yield('titolo-pagina')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/style.css') }}">
 </head>
-<body>
+<body data-bs-theme="dark">
     <header>
         @include('components.header')
     </header>
-    <main class="container">
+    <main class="container ">
         @yield('contenuto-main')
     </main>
     <footer>
